2fddb029497004e1e60a83e375bde64af9cee9c3
Luiz <lgwakano@gmail.com>
2023-10-18 05:03:48 -0600

    import api searchImages into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
 
 function App() {
 
     const handleSubmit = (term) => {
         console.log("search", term);
+        var results = searchImages(term);
     };
 
     return (<div>
